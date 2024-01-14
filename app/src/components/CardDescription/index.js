691c560f22068c75bdafe32fffa7917c3a2ec6a9
Quân Phạm Anh <anhquanhdtl@gmail.com>
2024-01-14 23:45:37 +0700

    update page intro

--- app/src/components/CardDescription/index.js
+++ app/src/components/CardDescription/index.js
@@ -2,13 +2,28 @@ import React from 'react';
 import './stylecarddes.css';
 import { ArrowRight } from '../ListIcon/Icon';
 
-const index = ({ width, height, arrow, desc, urlImage, borderRadius, color, fontSize }) => {
+const index = ({ width, height, arrow, desc, urlImage, borderRadius, color, fontSize, buttonleft, buttonright, bold }) => {
     return (
-        <div style={{ width: width || '100%', height: height || '100%', position: 'relative' }} className="image-container">
-            <img style={{ width: '100%', height: '100%', borderRadius: borderRadius, objectFit: 'cover', position: 'absolute', transition: 'transform 0.3s' }} src={urlImage} className="hover-image" />
-            <div style={{ display: 'flex', alignItems: 'baseline' }}>
-                <p style={{ position: 'absolute', bottom: '20px', left: '30px', color: color || 'black', fontWeight: 'bold' }}>The new Paramera</p>
-                <div style={{ position: 'absolute', bottom: '31px', right: '50px', color: color || 'black' }} className='btnArr'><ArrowRight /></div>
+        <div style={{ width: width || '100%', height: height || '100%', position: 'relative' }} >
+            <img className="image-container " style={{ width: '100%', height: '100%', borderRadius: borderRadius, objectFit: 'cover', position: 'absolute', transition: 'transform 0.3s' }} src={urlImage} />
+            <div className='grid_desc' style={{ color: color || 'white', fontWeight: bold }}>
+                <p className='label-desc' style={{ fontSize: fontSize || '20px' }} >{desc}</p>
+                {
+                    buttonleft && (
+                        <div className='btndesc1'>Build your 718</div>
+
+                    )
+                }
+                {
+                    buttonright && (
+                        <div className='btndesc2' >Build your 718</div>
+                    )
+                }
+                {
+                    arrow && (
+                        <div className='arrow btnArr'><ArrowRight /></div>
+                    )
+                }
             </div>
         </div>
     )
--- app/src/containers/IntroducingContainerPage/HomePageIntroduce/index.js
+++ app/src/containers/IntroducingContainerPage/HomePageIntroduce/index.js
@@ -6,7 +6,7 @@ import slide1 from '../../../../images/ListImage/slide.jpg'
 import slide2 from '../../../../images/ListImage/slide_2.jpg'
 import panamera_teaser_0 from '../../../../images/ListImage/panamera_teaser_0.jpg'
 import porsche_sunrise from '../../../../images/ListImage/porsche_sunrise.jpg'
-
+import CardDesc from '../../../components/CardDescription/index';
 
 
 function index() {
@@ -25,6 +25,7 @@ function index() {
                     </div>
                 </div>
                 <div className='slide-product'>
+                    <p style={{ color: 'white', position: 'absolute', top: '50%', left: '12%', fontWeight: 'bold', fontSize: '30px' }}><span>THE NEW PORSCHE PARAMERA.</span><br></br> <span>COURAGE CHANGES EVERYTHING.</span></p>
                     <div className='wrap-img'>
                         <img className='img' src={panamera_teaser_0} />
                     </div>
@@ -34,64 +35,74 @@ function index() {
 
             <div className='concert'>
                 <div className='my-favorite'>
-                    <img className='card' src={porsche_911} />
+                    <CardDesc urlImage={porsche_sunrise} width='100%' height='300px' borderRadius='15px' color='white' desc='The new Paramera' />
                 </div>
                 <div className='the-new-paramera'>
-                    <img className='card' src={slide1} />
+                    <CardDesc urlImage={slide1} width='100%' height='300px' borderRadius='15px' color='white' desc='Xe này của' />
+
 
                 </div>
                 <div className='cayenne'>
-                    <img className='card' src={porsche_911} />
+                    <CardDesc urlImage={slide1} width='100%' height='300px' borderRadius='15px' color='white' desc='Porsche chứ không phải của tôi!' />
 
                 </div>
             </div>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '50px', fontWeight: 'bold' }}>Models</div>
             <div className='models'>
-                <div className='card-models'>
-                    <img className='img-model' src={porsche_sunrise} />
+                <div className='card-models model_car1'>
+                    <CardDesc urlImage={slide1} width='100%' height='700px' borderRadius='15px' color='white' desc='Porsche 911!' buttonleft buttonright />
+
                 </div>
-                <div className='card-models1'>
-                    <img className='img-model' src={porsche_sunrise} />
+                <div className='card-models1 model_car1'>
+                    <CardDesc urlImage={slide1} width='100%' height='700px' borderRadius='15px' color='white' desc='Porsche 718!' buttonleft buttonright />
+
                 </div>
-                <div className='card-models'>
-                    <img className='img-model' src={porsche_sunrise} />
+                <div className='card-models model_car2'>
+                    <CardDesc urlImage={slide1} width='100%' height='700px' borderRadius='15px' color='white' desc='Porsche Macan!' buttonleft buttonright />
+
                 </div>
-                <div className='card-models1'>
-                    <img className='img-model' src={porsche_sunrise} />
+                <div className='card-models1 model_car2'>
+                    <CardDesc urlImage={slide1} width='100%' height='700px' borderRadius='15px' color='white' desc='Porsche Taycan!' buttonleft buttonright />
+
                 </div>
-                <div className='card-models'>
-                    <img className='img-model' src={porsche_sunrise} />
+                <div className='card-models model_car3'>
+                    <CardDesc urlImage={slide1} width='100%' height='700px' borderRadius='15px' color='white' desc='Porsche Paramera!' buttonleft buttonright />
+
                 </div>
-                <div className='card-models1'>
-                    <img className='img-model' src={porsche_sunrise} />
+                <div className='card-models1 model_car3'>
+                    <CardDesc urlImage={slide1} width='100%' height='700px' borderRadius='15px' color='white' desc='Porsche Cayyene!' buttonleft buttonright />
+
                 </div>
             </div>
-            <div className='social-media-stories'>
-                <div className='item1'>
-                    <img className='card-media' src={slide2} />
-                </div>
-                <div className='item2'>
-                    <img className='card-media' src={slide2} />
-                </div>
-                <div className='item3'>
-                    <img className='card-media' src={slide2} />
-                </div>
-                <div className='item4'>
-                    <img className='card-media' src={slide2} />
-                </div>
-                <div className='item5'>
-                    <img className='card-media' src={slide2} />
-                </div>
-                <div className='item6'>
-                    <img className='card-media' src={slide2} />
-                </div>
-                <div className='item7'>
-                    <img className='card-media' src={slide2} />
-                </div>
-                <div className='item8'>
-                    <img className='card-media' src={slide2} />
-                </div>
-                <div className='item9'>
-                    <img className='card-media' src={slide2} />
+            <div className='wrap-social'>
+                <div className='social-media-stories'>
+                    <div className='item1'>
+                        <img className='card-media' src={slide2} />
+                    </div>
+                    <div className='item2'>
+                        <img className='card-media' src={slide2} />
+                    </div>
+                    <div className='item3'>
+                        <img className='card-media' src={slide2} />
+                    </div>
+                    <div className='item4'>
+                        <img className='card-media' src={slide2} />
+                    </div>
+                    <div className='item5'>
+                        <img className='card-media' src={slide2} />
+                    </div>
+                    <div className='item6'>
+                        <img className='card-media' src={slide2} />
+                    </div>
+                    <div className='item7'>
+                        <img className='card-media' src={slide2} />
+                    </div>
+                    <div className='item8'>
+                        <img className='card-media' src={slide2} />
+                    </div>
+                    <div className='item9'>
+                        <img className='card-media' src={slide2} />
+                    </div>
                 </div>
             </div>
             <div className='discover'>
