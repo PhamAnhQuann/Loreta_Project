bd5b5bd832f8b71b98b7c4e42a5c7753f5b99440
Quân Phạm Anh <anhquanhdtl@gmail.com>
2024-01-14 09:37:37 +0700

    components  carddesc

--- app/src/components/CardDescription/index.js
+++ app/src/components/CardDescription/index.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import './stylecarddes.css';
-import { makeStyles } from '@mui/material';
-const useStyles = makeStyles(() => ({
-    color_text: {
-        color: 'black'
-    },
-    size_image: {
-        width: '100%',
-        height: '100%',
-        borderRadius: '15px'
-    }
-}));
-const index = ({ width, height, arrow, desc, urlImage }) => {
-    const classes = useStyles();
+import { ArrowRight } from '../ListIcon/Icon';
+
+const index = ({ width, height, arrow, desc, urlImage, borderRadius, color, fontSize }) => {
     return (
-        <div style={{ width: width || '100%', height: height || '100%' }} className='container'>
-            <div>
-                <img className={classes.size_image} src={urlImage} />
+        <div style={{ width: width || '100%', height: height || '100%', position: 'relative' }} className="image-container">
+            <img style={{ width: '100%', height: '100%', borderRadius: borderRadius, objectFit: 'cover', position: 'absolute', transition: 'transform 0.3s' }} src={urlImage} className="hover-image" />
+            <div style={{ display: 'flex', alignItems: 'baseline' }}>
+                <p style={{ position: 'absolute', bottom: '20px', left: '30px', color: color || 'black', fontWeight: 'bold' }}>The new Paramera</p>
+                <div style={{ position: 'absolute', bottom: '31px', right: '50px', color: color || 'black' }} className='btnArr'><ArrowRight /></div>
             </div>
         </div>
     )
